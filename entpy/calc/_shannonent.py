58f11b6ff20669333da24820e8de581828f14906
James Thorniley <james.thorniley@gmail.com>
2019-11-30 07:31:44 +0000

    Fix some edge cases in the shannonent function

--- entpy/calc/_shannonent.py
+++ entpy/calc/_shannonent.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from _entpy_calc import shannonent_bits, shannonent_nats, shannonent_base
 
-def shannonent(data: np.ndarray, units: str = 'bits', 
+def shannonent(data: np.ndarray, units: str = 'bits',
                implementation: str = 'cpp'):
     r"""Estimate Shannon entropy from data.
 
@@ -13,7 +13,7 @@ def shannonent(data: np.ndarray, units: str = 'bits',
 
         H(X) = -\sum_{x \in \mathcal{X}} p(x) \log p(x)
 
-    Where each :math:`x` is a discrete symbol in the support set 
+    Where each :math:`x` is a discrete symbol in the support set
     :math:`\mathcal{X}`. The simple estimator is obtained by calculating
     the relative frequency of each symbol :math:`f(x) = \frac{n(x)}
     {\sum_{x \in \mathcal{X}}n(x)}` and using that as an estimate of the
@@ -22,18 +22,18 @@ def shannonent(data: np.ndarray, units: str = 'bits',
     .. math::
 
         \hat{H}(X) = -\sum_{x \in \mathcal{X}} f(x) \log f(x)
- 
+
     Parameters
     ----------
 
         data : array_like or iterable
             The data to estimate from. This must be a discrete dtype
             (Shannon entropy does not make sense on floating point
-            values, but you can use integers, boolean, or string 
+            values, but you can use integers, boolean, or string
             symbols.)
         units : {"bits", "nats", float}
             Specify the units.
-            
+
             * ``"bits"`` will take the logarithm to base 2
             * ``"nats"`` will use the natual logarithm (base :math:`e`)
             * Some other base (specify as ``float``)
@@ -47,23 +47,25 @@ def shannonent(data: np.ndarray, units: str = 'bits',
     Examples
     --------
 
-        >>> import entpy.calc
-        >>> entpy.calc.shannonent([0, 1] * 20, 'bits')
+        >>> shannonent([0, 1] * 20, 'bits')
         1.0
-        >>> entpy.calc.shannonent([0, 1] * 20, 'nats')
+        >>> shannonent([0, 1] * 20, 'nats')
         0.6931471805599453
-        >>> entpy.calc.shannonent([0, 1] * 20, 2)
+        >>> shannonent([0, 1] * 20, 2)
         1.0
-        >>> entpy.calc.shannonent("1234")
+        >>> shannonent('1234')
         2.0
-
+        >>> shannonent([])
+        0.0
+        >>> shannonent('')
+        0.0
+        >>> shannonent([0.0, 1.0])
+        Traceback (most recent call last):
+          ...
+        TypeError: Data type float64 is not discrete
     """
 
     data = np.array(data, copy=False).flatten()
-    if data.shape[0] == 0:
-        # Empty array has zero entropy (return early to avoid
-        # type checking)
-        return 0.0
     if data.shape[0] == 1:
         # There is only one element in the array. If that element
         # is iterable (e.g. it is a string) assume we want the entropy
@@ -75,6 +77,11 @@ def shannonent(data: np.ndarray, units: str = 'bits',
             pass
         else:
             data = iter_data
+    if data.shape[0] == 0:
+        # Empty array has zero entropy (return early to avoid
+        # type checking)
+        return 0.0
+
     data_dtype = data.dtype
     if np.issubdtype(data_dtype, np.inexact):
         raise TypeError(f"Data type {data_dtype} is not discrete")
@@ -86,7 +93,7 @@ def shannonent(data: np.ndarray, units: str = 'bits',
         else:
             if units <= 0.0:
                 raise ValueError("numeric units must be positive")
-    
+
     if implementation == "python":
         return _shannonent_py(data, units)
 
@@ -97,7 +104,7 @@ def shannonent(data: np.ndarray, units: str = 'bits',
     else:
         return shannonent_base(data, units)
 
-    
+
 def _shannonent_py(data, units):
     """Simple numpy implementation"""
 
